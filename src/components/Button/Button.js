367f54dfc4288019ca3e24bb4b7f0db9bb5a0b6b
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-03-14 16:43:11 +0100

    setting up stroybook

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = (props) => {
+
+    const { variant = 'primary', children, ...rest } = props}
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
