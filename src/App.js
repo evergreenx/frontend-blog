8a328b7dbc14d38ee932f7a27a69d63aecbb33ba
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-03-15 19:13:35 +0100

    setting up resusable layout structur

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container } from './components/Styles/Container.styled';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Container>
+     
+     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, provident. Officia, doloremque. Nisi, facere ipsa doloremque necessitatibus eos asperiores ea quam, repudiandae accusantium quidem vel, minima molestiae ut laborum ab.
+     </Container>
+
+     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere aperiam rem vel itaque. Eos dolorem adipisci doloribus, error, hic debitis voluptatum sapiente illum quibusdam quam obcaecati pariatur. Praesentium, assumenda officia.
     </div>
   );
 }
---
+++ src/components/Layout/Header/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi modi nobis minima inventore iste, ab fuga, earum, fugit recusandae quidem corporis neque laborum consequatur facilis assumenda officia quibusdam eius vitae.
+    
+    </>
+  )
+}
+
+export default Index
